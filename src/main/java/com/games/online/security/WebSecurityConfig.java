badf2cd2734d059a9c8d5998187d7f05d17cb311
NabilTheNoise <dragon96nfs@gmail.com>
2021-04-06 14:42:19 +0430

    Optimized imports

--- src/main/java/com/games/online/security/WebSecurityConfig.java
+++ src/main/java/com/games/online/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.games.online.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
