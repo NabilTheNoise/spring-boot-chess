68fd942e8fbc4d7b5f6d912de864d8238b0dc804
NabilTheNoise <dragon96nfs@gmail.com>
2021-04-06 15:13:18 +0430

    UserDetailsServiceImpl added

---
+++ src/main/java/com/games/online/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.games.online.security.services;
+
+import com.games.online.models.User;
+import com.games.online.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException(String.format("Username %s not found!", username)));
+        return UserDetailsImpl.build(user);
+    }
+}
